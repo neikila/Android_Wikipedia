844cbafdec9603e256da3bf54251d380f207842e
Kirill <neikila@gmail.com>
2015-12-08 18:41:34 +0300

    Это наконец-то работает!

--- app/src/main/java/ru/mail/park/android_wikipedia/MainActivity.java
+++ app/src/main/java/ru/mail/park/android_wikipedia/MainActivity.java
@@ -3,8 +3,14 @@ package ru.mail.park.android_wikipedia;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,19 +18,40 @@ import dbservice.DbService;
 import ru.mail.park.android_wikipedia.fragments.ArticleFragment;
 import wikipedia.Article;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
     private DbService dbService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         dbService = ((ApplicationModified) getApplication()).getDbService();
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_activity);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         new GetArticleAsyncTask().execute();
-        new GetArticleAsyncTask().execute();
-        new GetArticleAsyncTask().execute();
-        new GetArticleAsyncTask().execute();
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_activity);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -56,6 +83,32 @@ public class MainActivity extends FragmentActivity {
         return true;
     }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camara) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.main_activity);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
     private class GetArticleAsyncTask extends AsyncTask<String, Void, Void> {
         private Article article;
 
@@ -75,17 +128,6 @@ public class MainActivity extends FragmentActivity {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.add(R.id.articles_container, ArticleFragment.newInstance(article.getTitle()));
             transaction.commit();
-//            ((TextView)findViewById(R.id.main_article_title)).setText(article.getTitle());
-//
-//            Button showArticle = (Button) findViewById(R.id.show_article);
-//            showArticle.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    Intent i = new Intent(MainActivity.this, ArticleActivity.class);
-//                    i.putExtra(ArticleActivity.ARTICLE_TITLE_TAG, article.getTitle());
-//                    startActivity(i);
-//                }
-//            });
         }
     }
 }
