8a81865c059780eacb396f707be4e5bdcdc23fab
Ivan Linnik <ivan_linnik@mail.ru>
2015-12-15 13:02:12 +0300

    Add first version of Contract classes
    Add fields and  create/drop table code

---
+++ app/src/main/java/dbservice/Contracts/ArticleContract.java
@@ -0,0 +1,33 @@
+package dbservice.Contracts;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ivan on 15.12.15.
+ */
+public final class ArticleContract {
+
+    public ArticleContract() {}
+
+    private static abstract class ArticleEntry implements BaseColumns {
+        public static final String TABLE_NAME = "Article";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_BODY = "body";
+        public static final String COLUMN_NAME_logo = "logo";
+    }
+
+    private static final String TEXT_TYPE = "TEXT";
+    private static final String COMMA_SEP = ",";
+
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + ArticleEntry.TABLE_NAME + " (" +
+            ArticleEntry._ID + " INTEGER PRIMARY KEY," + COMMA_SEP +
+            ArticleEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+            ArticleEntry.COLUMN_NAME_BODY + TEXT_TYPE + COMMA_SEP +
+            ArticleEntry.COLUMN_NAME_logo + TEXT_TYPE + " )";
+
+    public static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXIST " + ArticleEntry.TABLE_NAME;
+
+    //todo add methods
+}
---
+++ app/src/main/java/dbservice/Contracts/HistoryOfSearchContract.java
@@ -0,0 +1,32 @@
+package dbservice.Contracts;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by ivan on 15.12.15.
+ */
+public final class HistoryOfSearchContract {
+    public HistoryOfSearchContract() {}
+
+    private static abstract class HistoryOfSearchEntry implements BaseColumns {
+        public static final String TABLE_NAME = "HistoryOfSearch";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_BODY = "link";
+        public static final String COLUMN_NAME_logo = "logo";
+    }
+
+    private static final String TEXT_TYPE = "TEXT";
+    private static final String COMMA_SEP = ",";
+
+    public static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + HistoryOfSearchEntry.TABLE_NAME + " (" +
+                    HistoryOfSearchEntry._ID + " INTEGER PRIMARY KEY," + COMMA_SEP +
+                    HistoryOfSearchEntry.COLUMN_NAME_TITLE + TEXT_TYPE + COMMA_SEP +
+                    HistoryOfSearchEntry.COLUMN_NAME_BODY + TEXT_TYPE + COMMA_SEP +
+                    HistoryOfSearchEntry.COLUMN_NAME_logo + TEXT_TYPE + " )";
+
+    public static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXIST " + HistoryOfSearchEntry.TABLE_NAME;
+
+    //todo add methods
+}
