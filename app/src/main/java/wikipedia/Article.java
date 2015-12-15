e6a743c14775e62149ee2c34d2b3a8cbd59237d9
Ivan Linnik <ivan_linnik@mail.ru>
2015-12-15 13:01:21 +0300

    Change Article
    Add logo field to Article class
    Change Article constructor and it calls

--- app/src/main/java/wikipedia/Article.java
+++ app/src/main/java/wikipedia/Article.java
@@ -5,12 +5,18 @@ package wikipedia;
  */
 public class Article {
     private String title;
+    private String logo = "";
 
-    public Article(String title) {
+    public Article(String title, String logo) {
         this.title = title;
+        this.logo = logo;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public String getLogo() {
+        return logo;
     }
 }
