9ff9f0d11ed7892588974f0921850224f98dc52d
Ivan Linnik <ivan_linnik@mail.ru>
2015-12-23 13:08:44 +0300

    Add history fragment

--- app/src/main/java/ru/mail/park/android_wikipedia/fragments/HistoryFragment.java
+++ app/src/main/java/ru/mail/park/android_wikipedia/fragments/HistoryFragment.java
@@ -3,12 +3,15 @@ package ru.mail.park.android_wikipedia.fragments;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
@@ -16,13 +19,18 @@ import com.squareup.otto.Subscribe;
 import java.util.List;
 
 import ru.mail.park.android_wikipedia.ApplicationModified;
+import ru.mail.park.android_wikipedia.ArticlesAdapter;
+import ru.mail.park.android_wikipedia.BaseActivity;
 import ru.mail.park.android_wikipedia.R;
 import ru.mail.park.android_wikipedia.ServiceHelper;
+import utils.OttoMessage;
 import utils.ResultArticle;
 import wikipedia.Article;
 
 public class HistoryFragment extends Fragment {
     private Handler handler;
+    private RecyclerView recList;
+    private static List<Article> articlesList;
 
     public static HistoryFragment newInstance() {
         HistoryFragment fragment = new HistoryFragment();
@@ -38,24 +46,42 @@ public class HistoryFragment extends Fragment {
     @Subscribe
     public void react(final ResultArticle message) {
         if (handler != null) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    ListItemTranslationAdapter adapter = new ListItemTranslationAdapter(message.getArticles());
-                    ListView list = (ListView) getActivity().findViewById(R.id.listWords);
-                    list.setAdapter(adapter);
-                }
-            });
+            if (message.getMessageType().equals(OttoMessage.MessageType.ResultArticle)) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ArticlesAdapter adapter = (ArticlesAdapter) recList.getAdapter();
+                        articlesList = ((ResultArticle) message).getArticles();
+                        adapter.setArticles(articlesList);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            } else if (message.getMessageType().equals(OttoMessage.MessageType.UpdateAdapter)) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ArticlesAdapter adapter = (ArticlesAdapter) recList.getAdapter();
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            } else if (message.getMessageType().equals(OttoMessage.MessageType.NoResult)) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), "No article found", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        handler = new Handler();
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
             // TODO пока тут ничего делать
         }
-        handler = new Handler();
     }
 
     @Override
@@ -64,8 +90,29 @@ public class HistoryFragment extends Fragment {
         Bus bus = ((ApplicationModified) getActivity().getApplication()).getBus();
         bus.register(this);
 
-        new ServiceHelper().getArticlesFromHistory(getActivity());
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        View myFragment = inflater.inflate(R.layout.fragment_main, container, false);
+        recList = (RecyclerView) myFragment.findViewById(R.id.card_list);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this.getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = ((TextView) v.findViewById(R.id.title)).getText().toString();
+                ((BaseActivity) getActivity()).openArticle(title);
+            }
+        };
+        ArticlesAdapter articlesAdapter;
+        if (articlesList == null) {
+            articlesAdapter = new ArticlesAdapter(listener);
+            new ServiceHelper().getArticlesFromHistory(this.getActivity());
+        } else {
+            articlesAdapter = new ArticlesAdapter(listener, articlesList);
+        }
+        recList.setAdapter(articlesAdapter);
+
+        return myFragment;
     }
 
     private class ListItemTranslationAdapter extends ArrayAdapter<Article> {
