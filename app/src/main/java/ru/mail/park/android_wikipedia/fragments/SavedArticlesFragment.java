0c788996f71f24cc5888a55b96b3359dfcc8c317
Ivan Semenov <semenov@bmail.ru>
2015-12-23 13:00:40 +0300

    Add base for saved_articls fragment

--- app/src/main/java/ru/mail/park/android_wikipedia/fragments/SavedArticlesFragment.java
+++ app/src/main/java/ru/mail/park/android_wikipedia/fragments/SavedArticlesFragment.java
@@ -3,12 +3,15 @@ package ru.mail.park.android_wikipedia.fragments;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
@@ -16,13 +19,17 @@ import com.squareup.otto.Subscribe;
 import java.util.List;
 
 import ru.mail.park.android_wikipedia.ApplicationModified;
+import ru.mail.park.android_wikipedia.ArticlesAdapter;
+import ru.mail.park.android_wikipedia.BaseActivity;
 import ru.mail.park.android_wikipedia.R;
 import ru.mail.park.android_wikipedia.ServiceHelper;
+import utils.OttoMessage;
 import utils.ResultArticle;
 import wikipedia.Article;
 
 public class SavedArticlesFragment extends Fragment {
     private Handler handler;
+    private RecyclerView recList;
 
     public static SavedArticlesFragment newInstance() {
         SavedArticlesFragment fragment = new SavedArticlesFragment();
@@ -36,16 +43,33 @@ public class SavedArticlesFragment extends Fragment {
     }
 
     @Subscribe
-    public void react(final ResultArticle message) {
+    public void react(final OttoMessage message) {
         if (handler != null) {
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    ListItemTranslationAdapter adapter = new ListItemTranslationAdapter(message.getArticles());
-                    ListView list = (ListView) getActivity().findViewById(R.id.listWords);
-                    list.setAdapter(adapter);
-                }
-            });
+            if (message.getMessageType().equals(OttoMessage.MessageType.ResultArticle)) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ArticlesAdapter adapter = (ArticlesAdapter) recList.getAdapter();
+                        adapter.setArticles(((ResultArticle) message).getArticles());
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            } else if (message.getMessageType().equals(OttoMessage.MessageType.UpdateAdapter)) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ArticlesAdapter adapter = (ArticlesAdapter) recList.getAdapter();
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+            } else if (message.getMessageType().equals(OttoMessage.MessageType.NoResult)) {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(), "No article found", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
         }
     }
 
@@ -62,7 +86,23 @@ public class SavedArticlesFragment extends Fragment {
                              Bundle savedInstanceState) {
         Bus bus = ((ApplicationModified) getActivity().getApplication()).getBus();
         bus.register(this);
-        // Inflate the layout for this fragment
+
+        View myFragment = inflater.inflate(R.layout.fragment_saved_articles, container, false);
+        recList = (RecyclerView) myFragment.findViewById(R.id.card_list);
+        recList.setHasFixedSize(true);
+        LinearLayoutManager llm = new LinearLayoutManager(this.getActivity());
+        llm.setOrientation(LinearLayoutManager.VERTICAL);
+        recList.setLayoutManager(llm);
+
+        ArticlesAdapter articlesAdapter = new ArticlesAdapter(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = ((TextView) v.findViewById(R.id.title)).getText().toString();
+                ((BaseActivity) getActivity()).openArticle(title);
+            }
+        });
+        recList.setAdapter(articlesAdapter);
+
         new ServiceHelper().getSavedArticles(getActivity());
         return inflater.inflate(R.layout.fragment_saved_articles, container, false);
     }
